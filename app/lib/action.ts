662bdca6c39ad44ced18cec001fdb3781c5ce9cf
Neeraja <neeraja.ug@livares.com>
2024-10-18 12:20:10 +0530

    authentication and metadata

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -64,6 +67,23 @@ export async function deleteInvoice(id: string) {
       message: 'Database Error: Failed to delete the invoice',
     } 
   }
- 
+}
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
 }
 
